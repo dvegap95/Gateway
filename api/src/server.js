5658e9c13c527b40207fc7a7e09f1d34bb3c3bb5
dvegap95 <dvegap95@gmail.com>
2022-05-27 23:03:11 -0400

    serve client static content with spa fallback

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const express = require('express');
-
+const gatewayRouter = require("./routes/gatewayRoutes");
+const path = require("path")
+const resolve = path.resolve
 
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
@@ -11,10 +13,16 @@ db.on('open',()=>{});
 
 const app = express();
 
+app.use(express.static(process.env.STATIC_CONTENT_PATH));
+
 app.use(express.json());
 
-const gatewayRouter = require("./routes/gatewayRoutes");
 
 app.use("/api/gateways",gatewayRouter);
+
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(resolve(process.env.STATIC_CONTENT_PATH), "index.html"));
+});
 
 module.exports = app;
