cb001e41af6d5eceda85910e8c5663d6872b9042
dvegap95 <dvegap95@gmail.com>
2022-05-29 11:37:53 -0400

    setup client routes and notification systems

---
+++ client/src/utils/toast.ts
@@ -0,0 +1,16 @@
+import { toast as _toast, ToastOptions } from 'react-toastify';
+
+let defaultOptions:ToastOptions = {
+    type:'success',
+    position: "bottom-right",
+    autoClose:2000,
+}
+
+
+export default function toast(str:string,options:ToastOptions | undefined = {}){
+    return _toast(str,{...defaultOptions,...options});
+}
+
+export function errorToast(str:string,options:ToastOptions | undefined = {}){
+    return _toast(str,{...defaultOptions,autoClose:3000,...options,type:"error"});
+}
---
+++ client/src/views/GatewaysView.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GatewaysView() {
+  return (
+    <div>Gateways View</div>
+  )
+}
