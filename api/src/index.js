b97e3d27856971f570116344d2362f3d0eb864ff
dvegap95 <dvegap95@gmail.com>
2022-06-02 15:33:40 -0400

    environment for alwaysdata deployment

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 require('dotenv').config({path:path.resolve(process.cwd(),"prod.env")});
 const server = require("./server");
 
-const port = process.env.PORT || 3001; 
-const host = process.env.HOST || 'localhost'; 
+const port = process.env.PORT || process.env.ALWAYSDATA_HTTPD_PORT || 3001; 
+const host = process.env.HOST || process.env.ALWAYSDATA_HTTPD_IP || 'localhost'; 
 
 server.listen(port,host,()=>console.log(`server started at port ${port}`));
--- api/src/server.js
+++ api/src/server.js
@@ -1,41 +1,45 @@
 const mongoose = require("mongoose");
-const express = require('express');
+const express = require("express");
 const gatewayRouter = require("./routes/gatewayRoutes");
 const peripheralDeviceRouter = require("./routes/peripheralDeviceRoutes");
-const path = require("path")
-const resolve = path.resolve
+const path = require("path");
+const resolve = path.resolve;
 const nocache = require("nocache");
 
-//connect to environment-defined database 
+//connect to environment-defined database
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 
 //handle db error
-db.on('error',(e)=>{
-    console.error(e);
-    process.exit(1)
+db.on("error", (e) => {
+  console.error(e);
+  process.exit(1);
 });
 
 const app = express();
 
+const static_path = path.resolve(
+  process.env.STATIC_CONTENT_PATH || "../client/dist"
+);
+
 //serve static content from environment-defined path
 //should be client/dist path (client distribution folder)
-app.use(express.static(process.env.STATIC_CONTENT_PATH||"../client/dist"));
+app.use(express.static(static_path));
 
-app.use(express.json());//json body objects
-app.use("/api",nocache());//disable cache for api
+app.use(express.json()); //json body objects
+app.use("/api", nocache()); //disable cache for api
 
 //add routers
-app.use("/api/gateways",gatewayRouter);
-app.use("/api/peripheral-devices",peripheralDeviceRouter);
+app.use("/api/gateways", gatewayRouter);
+app.use("/api/peripheral-devices", peripheralDeviceRouter);
 
 //api fallback distinct from global fallback
-app.use("/api",(req, res, next) => {
-    res.status(404).json({message:"Api not found"})
+app.use("/api", (req, res, next) => {
+  res.status(404).json({ message: "Api not found" });
 });
 //global fallback (static content) to index.html to serve SPA
 app.use((req, res, next) => {
-    res.sendFile(path.join(resolve(process.env.STATIC_CONTENT_PATH), "index.html"));
+  res.sendFile(path.join(static_path, "index.html"));
 });
 
 module.exports = app;
